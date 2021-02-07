08550fd74eb838bb6b8a5130d63824f2b4ca1293
margaretbarseghayn <margaretbarseghyan@gmail.com>
2021-02-07 15:26:06 +0400

    Iterator for Fibonacci series

---
+++ fibonacci.js
@@ -0,0 +1,26 @@
+let fibonacci = {};
+
+fibonacci[Symbol.iterator] = function (){
+const fib = [0,1];
+let i = 2;
+    return{
+        next: () =>{
+            if(i >= 2){
+                fib[i] = fib[i - 1] + fib[i - 2]
+            }
+            return{
+                done: i === 10,
+                value: fib[i++ -2]
+            }
+        }
+    }
+}
+
+// const iterator = fibonacci[Symbol.iterator]()
+// console.log('value', iterator.next().value);
+
+let i = 0;
+for (const fibonacciElement of fibonacci) {
+    if(i++ > 10) break;
+    console.log(fibonacciElement)
+}
